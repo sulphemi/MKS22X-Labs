fef70de754a45f8cca4c72a08654e566ee81e3dc
Jing Qian <jqian30@stuy.edu>
2022-02-03 14:42:32 -0500

    copy and pasted more code wheeeeeee

--- 01-Recursion/Recursion.java
+++ 01-Recursion/Recursion.java
@@ -21,6 +21,29 @@ public class Recursion {
     }
   }
 
+  /*Print all words that are made of the characters in the array of letters.
+  *There may not be consecutive equal letters, so:
+  *aax is not allowed, but axa is allowed.
+  *@param length : the length of the words that are to be printed
+  *@param letters: the letters you should be using,
+  *@precondition: letters contains at least 2 characters, and has no duplicates.
+  */
+  public static void printNoDoubleLetterWords(int length,char[] letters){
+    printNoDoubleLetterWords(length,"",letters);
+  }
+
+  /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
+  *aax is not allowed, but axa is allowed.
+  *@param length : either how many more letters need to be
+  *@param word   : the partial word so far.
+  *@param letters: the letters you should be using
+  */
+  public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+    //WRITE THIS METHOD
+  }
+
+
+  /*** MAIN ***/
   public static void main(String[] args) {
     printAllWords(Integer.parseInt(args[0]));
   }
