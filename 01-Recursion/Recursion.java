6c2bf0e0f5850fc77631b863610d00cc1a6637e1
Jing Qian <jqian30@stuy.edu>
2022-02-03 14:27:25 -0500

    yoinked code from website

---
+++ 01-Recursion/Recursion.java
@@ -0,0 +1,14 @@
+/*Print all words that are made of the letters a-e inclusive.
+*@param length : the length of the words that are to be printed
+*/
+public static void printAllWords(int length){
+  printAllWords(length,"");
+}
+
+/*Print all words that are made of the letters a-e inclusive.
+*@param length : either how many more letters or the total length depending on implementation
+*@param word   : the partial word so far.
+*/
+public static void printAllWords(int length,String word){
+  //WRITE THIS METHOD
+}
