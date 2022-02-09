fd57df0fa6e3492d832c3db1eaca3a63dd9c4f1f
Jing Qian <jqian30@stuy.edu>
2022-02-09 14:23:46 -0500

    moved partialSum to classwork

--- 01-Recursion/Recursion.java
+++ 01-Recursion/Recursion.java
@@ -132,36 +132,8 @@ public class Recursion {
   }
 
 
-  //returns if partial sum is possible
-  public static boolean partialSum(int start, int[] arr, int target, int sum) {
-    //base case
-    if (sum == target) {
-      return true;
-    }
-    if (start == arr.length) {
-      //means we reached the end without being able to partial sum thingy thing thing
-      return false;
-    }
-
-    return (
-      partialSum(start + 1, arr, target, sum + arr[start]) ||     //with current index
-      partialSum(start + 1, arr, target, sum)                     //without current index
-      );
-  }
-
-
-
   /*** MAIN ***/
   public static void main(String[] args) {
-    //char[] letters = {'a', 'b', 'c'};
-    //printNoDoubleLetterWords(Integer.parseInt(args[0]), letters);
 
-    int[] arr = new int[args.length];
-
-    for (int i = 0; i < args.length; i++) {
-      arr[i] = Integer.parseInt(args[i]);
-    }
-
-    System.out.println(partialSum(0, arr, 10, 0));
   }
 }
