59b7e87fb0664398a5045c38ee90ae833ac0f37b
Jing Qian <jqian30@stuy.edu>
2022-02-10 14:26:15 -0500

    header for groupNoAdj

--- classwork/RecursionClasswork.java
+++ classwork/RecursionClasswork.java
@@ -49,6 +49,15 @@ public class RecursionClasswork {
     }
   }
 
+  //Given an array of ints, is it possible to choose a group of some of the ints,
+  //such that the group sums to the given target with this additional constraint:
+  //If a value in the array is chosen to be in the group, the value immediately
+  //following it in the array must not be chosen. (No loops needed.)
+  public static boolean groupNoAdj(int start, int[] nums, int target) {
+    
+  }
+
+
   public static void main(String[] args) {
     //char[] letters = {'a', 'b', 'c'};
     //printNoDoubleLetterWords(Integer.parseInt(args[0]), letters);
