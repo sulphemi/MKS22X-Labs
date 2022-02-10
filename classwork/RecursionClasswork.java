93df10a1ceb385db6ada7882cabf6a89ffd74654
Jing Qian <jqian30@stuy.edu>
2022-02-10 14:35:29 -0500

    header for splitOdd10

--- classwork/RecursionClasswork.java
+++ classwork/RecursionClasswork.java
@@ -74,6 +74,17 @@ public class RecursionClasswork {
     return groupNoAdj(start, arr, target, 0, false);
   }
 
+  /*
+  Given an array of ints, is it possible to divide the ints into two groups,
+  so that the sum of one group is a multiple of 10, and the sum of the other
+  group is odd. Every int must be in one group or the other. Write a recursive
+  helper method that takes whatever arguments you like, and make the initial
+  call to your recursive helper from splitOdd10(). (No loops needed.)
+  */
+  public static boolean splitOdd10(int[] nums) {
+    
+  }
+
 
   public static void main(String[] args) {
     //char[] letters = {'a', 'b', 'c'};
