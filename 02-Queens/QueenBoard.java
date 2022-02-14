cb8e5fabfb7fe519f4a784f8dac7439f225c3b84
Jing Qian <jqian30@stuy.edu>
2022-02-14 14:27:56 -0500

    toString method

--- 02-Queens/QueenBoard.java
+++ 02-Queens/QueenBoard.java
@@ -8,13 +8,33 @@ public class QueenBoard {
   }
 
   /***** PRIVATE METHODS *****/
-  private boolean addQueen() {}
-  private boolean removeQueen() {}
-
+  // private boolean addQueen() {}
+  // private boolean removeQueen() {}
+  //
   /***** PUBLIC METHODS *****/
-  public String toString() {}
+  public String toString() {
+    String output = "";
 
-  public boolean solve() {} //wrapper method
+    for (int i = 0; i < board.length; i++) {
+      for (int k = 0; k < board.length; k++) {
+        output += board[i][k];
+        output += ' ';
+      }
+      if (i != board.length - 1) {
+        output += '\n'; //newline after row
+      }
+    }
 
-  public int countSolutions() {}
+    return output;
+  }
+  //
+  // public boolean solve() {} //wrapper method
+  //
+  // public int countSolutions() {}
+
+  /***** MAIN *****/
+  public static void main(String[] args) {
+    QueenBoard qb = new QueenBoard(8);
+    System.out.println(qb.toString());
+  }
 }
