a4da7f6d2e649af24005367d67f74204c5ef7c72
Jing Qian <jqian30@stuy.edu>
2022-02-14 14:38:56 -0500

    deepCopy method

--- 02-Queens/QueenBoard.java
+++ 02-Queens/QueenBoard.java
@@ -7,6 +7,10 @@ public class QueenBoard {
     board = new int[n][n];
   }
 
+  public QueenBoard(int[][] array) { //note that board will now be linked to array
+    board = array;
+  }
+
   /***** PRIVATE METHODS *****/
   // private boolean addQueen() {}
   // private boolean removeQueen() {}
@@ -32,9 +36,31 @@ public class QueenBoard {
   //
   // public int countSolutions() {}
 
+  public QueenBoard deepCopy() {
+    //copy board to fresh int[][]
+    int[][] newBoard = new int[board.length][board.length];
+
+    for (int i = 0; i < board.length; i++) {
+      for (int k = 0; k < board.length; k++) {
+        newBoard[i][k] = board[i][k];
+      }
+    }
+
+    return new QueenBoard(newBoard);
+  }
+
   /***** MAIN *****/
   public static void main(String[] args) {
     QueenBoard qb = new QueenBoard(8);
+    QueenBoard qbclone = qb.deepCopy();
+    System.out.println("qb:");
     System.out.println(qb.toString());
+    System.out.println("copy:");
+    System.out.println(qbclone);
+    qb.board[0][0] = 5;
+    System.out.println("qb");
+    System.out.println(qb);
+    System.out.println("copy:");
+    System.out.println(qbclone);
   }
 }
