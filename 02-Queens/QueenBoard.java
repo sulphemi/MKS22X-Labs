d3ef8332ebf500abc80368b20926563bafe59dae
Jing Qian <jqian30@stuy.edu>
2022-02-14 14:22:29 -0500

    method headers

--- 02-Queens/QueenBoard.java
+++ 02-Queens/QueenBoard.java
@@ -6,4 +6,15 @@ public class QueenBoard {
   public QueenBoard(int n) { //initializes the board for size n
     board = new int[n][n];
   }
+
+  /***** PRIVATE METHODS *****/
+  private boolean addQueen() {}
+  private boolean removeQueen() {}
+
+  /***** PUBLIC METHODS *****/
+  public String toString() {}
+
+  public boolean solve() {} //wrapper method
+
+  public int countSolutions() {}
 }
