3aaf36c60e08ac1fd3735f3cac08b8316c8ba6b2
Jing Qian <jqian30@stuy.edu>
2022-02-14 14:18:23 -0500

    init QueenBoard.java

---
+++ 02-Queens/QueenBoard.java
@@ -0,0 +1,9 @@
+public class QueenBoard {
+  /***** FIELDS *****/
+  int[][] board;
+
+  /***** CONSTRUCTORS *****/
+  public QueenBoard(int n) { //initializes the board for size n
+    board = new int[n][n];
+  }
+}
