5a83b98ffe0f6a2d4822e9339405e5440a4460c0
Jing Qian <jqian30@stuy.edu>
2022-03-08 14:48:54 -0500

    mindless init

---
+++ 04-USACO/Bronze.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Bronze {
+  public static void main(String[] args) throws Exception {
+    Scanner Pumpkin = new Scanner(System.in);
+
+    final int R = Pumpkin.nextInt();
+    final int C = Pumpkin.nextInt();
+    final int E = Pumpkin.nextInt();
+    final int N = Pumpkin.nextInt();
+
+    int[][] map = new int[R][C];
+  }
+}
