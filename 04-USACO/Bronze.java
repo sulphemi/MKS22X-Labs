1cc0e9015b895d84b88c4e56d150e04fa5fb7ff5
Jing Qian <jqian30@stuy.edu>
2022-03-09 14:46:44 -0500

    stomp method

--- 04-USACO/Bronze.java
+++ 04-USACO/Bronze.java
@@ -20,6 +20,23 @@ public class Bronze {
   }
 
   public static void stomp(int[][] map, int row, int col, int depth) {
-    
+    //find highest elevation by iterating through
+    int highest = map[row][col];
+    for (int i = row; i < row + 3; i++) {
+      for (int k = col; k < col + 3; k++) {
+        if (map[i][k] > highest) {
+          highest = map[i][k];
+        }
+      }
+    }
+
+    //iterate through
+    for (int i = row; i < row + 3; i++) {
+      for (int k = col; k < col + 3; k++) {
+        if (map[i][k] == highest) {
+          map[i][k] -= depth;//lower
+        }
+      }
+    }
   }
 }
