863613b68607ead922339984163903d724652c7a
Jing Qian <jqian30@stuy.edu>
2022-03-22 14:35:49 -0400

    helpers and wrote merge in a less dooky way

--- 06-Merge/Merge.java
+++ 06-Merge/Merge.java
@@ -12,11 +12,9 @@ public class Merge {
         tray[leftie + righty] = left[leftie];
         leftie++;
         continue;
-      }
-      if (leftie == left.length || left[leftie] > right[righty]) {
+      } else if (leftie == left.length || left[leftie] > right[righty]) {
         tray[leftie + righty] = right[righty];
         righty++;
-        continue;
       }
     }
 
@@ -44,10 +42,22 @@ public class Merge {
   }
 
   public static void main(String[] args) {
-    int[] a = {1, 2, 3, 4, 5, 6};
-    int[] b = {1, 2, 3};
+    int[] a = {0, 0, 0, 3, 4, 5, 6, 7, 10, 999, 69420};
+    int[] b = {1, 2, 3, 727, 727};
 
     System.out.println(Arrays.toString(merge(a, b)));
-    System.out.println(Arrays.toString(dumbMerge(a, b)));
+    System.out.println(Arrays.equals(merge(a, b), dumbMerge(a, b)));
+  }
+
+  public static int randInt(int lower, int upper) {
+    return (int)(Math.random() * (upper - lower + 1) + lower);
+  }
+
+  public static int[] randArray(int size) {
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) {
+      array[i] = randInt(0, 10);
+    }
+    return array;
   }
 }
