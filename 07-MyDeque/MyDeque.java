deebce79337f54ba1f2fa85a175ab9b0128da00f
Jing Qian <jqian30@stuy.edu>
2022-03-29 15:03:32 -0400

    getIndex functionality

--- 07-MyDeque/MyDeque.java
+++ 07-MyDeque/MyDeque.java
@@ -22,7 +22,7 @@ public class MyDeque<E> {
 
   public String toStringDebug() {
     String output = "[";
-    for (int i = 0; ++i < data.length;) {
+    for (int i = 0; i < data.length; i++) {
       output += data[i];
       if (i != data.length - 1) {output += ", ";}
     }
@@ -32,19 +32,16 @@ public class MyDeque<E> {
 
   public String toString() {
     String output = "[";
-    int i = start + 1;
-    while (i != end) {
-      if (i == data.length) {i = 0;} //if we reached end of physical array, wrap back to beginning
-      output += data[i];
-      output += ", ";
-      i++;
+    for (int i = 0; i < size(); i++) {
+      output += data[getIndex(i)];
+      output += (i != size() - 1) ? ", " : "]";
     }
     return output;
   }
 
   //NTS: NEED A RESIZE THINGY
   public void addFirst(E element) {
-    data[start] = element; //
+    data[start] = element;
     start--;
     size++;
     //if start == end resize
@@ -60,24 +57,26 @@ public class MyDeque<E> {
   private void resize() {
     @SuppressWarnings("unchecked")
     E[] newThingy = (E[])new Object[data.length * 2];
+  }
 
-    int i = 0; //original array pointer
-    int k = 0; //new array pointer
-    //copy from arraystart to middle
-    while (i < start) {
-      newThingy[k] = data[i];
-      i++;
-      k++;
+  //abstraction go brrrr
+  //converts PERCEIVED INDEX (in deque) into ACTUAL INDEX (in data)
+  private int getIndex(int index) {
+    index += start + 1;
+    if (index >= data.length) {
+      //wrap
+      index -= data.length;
     }
-    //now i points to a null
-    i++; //increment i so it points to something again
-    //copy from middle to 
-    while (i < )
+    return index;
   }
 
   public static void main(String[] args) {
-    MyDeque<Object> aaaa = new MyDeque<Object>(10);
-    aaaa.end = 9;
+    MyDeque<String> aaaa = new MyDeque<String>(10);
+    String[] aa = {"3", "4", null, null, "1", "2"};
+    aaaa.data = aa;
+    aaaa.start = 3;
+    aaaa.end = 2;
+    aaaa.size = 4;
     System.out.println(aaaa.toStringDebug());
     System.out.println(aaaa.toString());
     System.out.println("start: " + aaaa.start);
