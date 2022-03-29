618eefcaa287de83b752283cdeac69fe3d0334b9
Jing Qian <jqian30@stuy.edu>
2022-03-29 16:27:44 -0400

    working prototype

--- 07-MyDeque/MyDeque.java
+++ 07-MyDeque/MyDeque.java
@@ -1,7 +1,13 @@
+import java.util.*;
+
 public class MyDeque<E> {
+  /***** FIELDS *****/
+
   private E[] data;
   private int size;
   private int start, end; //these both point to empty space
+
+  /***** CONSTRUCTORS *****/
 
   public MyDeque() {
     this(32); //initialCapacity defaults to 32
@@ -15,6 +21,58 @@ public class MyDeque<E> {
     start = data.length / 2;
     end = start + 1;
   }
+
+  /***** PRIVATE METHODS *****/
+
+  //doubles the size of data
+  private void resize() {
+    @SuppressWarnings("unchecked")
+    E[] newThingy = (E[])new Object[data.length * 2];
+
+    //fill the array from index 0 to whatever
+    for (int i = 0; i < size; i++) {
+      newThingy[i] = data[getIndex(i)];
+    }
+    //at this point, newThingy is an array filled with elements from the deque
+    //from index 0 to size() - 1
+    start = newThingy.length - 1; //the next space is at end
+    end = size; //next space is at size
+    data = newThingy; //overwrite memory address
+  }
+
+  //abstraction go brrrr
+  //converts PERCEIVED INDEX (in deque) into ACTUAL INDEX (in data)
+  private int getIndex(int index) {
+    index += start + 1;
+    if (index >= data.length) {
+      //wrap
+      index -= data.length;
+    }
+    return index;
+  }
+
+  //hopefully these methods wont go spoom too hard
+  private void decrementStart() {
+    start--;
+    if (start < 0) {start = data.length - 1;} //start wraps to end of array
+  }
+
+  private void incrementStart() {
+    start++;
+    if (start >= data.length) {start = 0;} //wraps to start
+  }
+
+  private void decrementEnd() {
+    end--;
+    if (end < 0) {end = data.length - 1;} //wraps to end
+  }
+
+  private void incrementEnd() {
+    end++;
+    if (end >= data.length) {end = 0;} //end wraps to beginning of array
+  }
+
+  /***** PUBLIC METHODS *****/
 
   public int size() {
     return size;
@@ -89,84 +147,5 @@ public class MyDeque<E> {
 
   public E getLast() {
     return data[getIndex(size - 1)];
-  }
-
-  //doubles the size of data
-  private void resize() {
-    @SuppressWarnings("unchecked")
-    E[] newThingy = (E[])new Object[data.length * 2];
-
-    //fill the array from index 0 to whatever
-    for (int i = 0; i < size; i++) {
-      newThingy[i] = data[getIndex(i)];
-    }
-    //at this point, newThingy is an array filled with elements from the deque
-    //from index 0 to size() - 1
-    start = newThingy.length - 1; //the next space is at end
-    end = size; //next space is at size
-    data = newThingy; //overwrite memory address
-  }
-
-  //abstraction go brrrr
-  //converts PERCEIVED INDEX (in deque) into ACTUAL INDEX (in data)
-  private int getIndex(int index) {
-    index += start + 1;
-    if (index >= data.length) {
-      //wrap
-      index -= data.length;
-    }
-    return index;
-  }
-
-  //hopefully these methods wont go spoom too hard
-  private void decrementStart() {
-    start--;
-    if (start < 0) {start = data.length - 1;} //start wraps to end of array
-  }
-
-  private void incrementStart() {
-    start++;
-    if (start >= data.length) {start = 0;} //wraps to start
-  }
-
-  private void decrementEnd() {
-    end--;
-    if (end < 0) {end = data.length - 1;} //wraps to end
-  }
-
-  private void incrementEnd() {
-    end++;
-    if (end >= data.length) {end = 0;} //end wraps to beginning of array
-  }
-
-  public static void main0(String[] args) {
-    MyDeque<String> aaaa = new MyDeque<String>(10);
-    String[] aa = {"3", "4", null, null, "1", "2"};
-    aaaa.data = aa;
-    aaaa.start = 3;
-    aaaa.end = 2;
-    aaaa.size = 4;
-    aaaa.addFirst("0");
-    aaaa.addLast("5");
-    System.out.println(aaaa.toStringDebug());
-    System.out.println(aaaa.toString());
-    System.out.println("start: " + aaaa.start);
-    System.out.println("end:   " + aaaa.end);
-  }
-
-  public static void main(String[] args) {
-    MyDeque<String> aaaa = new MyDeque<String>(2);
-    for (int i = 0; ++i<3;) {
-      aaaa.addLast("" + i);
-    }
-    System.out.println(aaaa);
-    for (int i = 0; ++i<40;) {
-      aaaa.addFirst("" + i);
-    }
-    System.out.println(aaaa);
-    for (int i = 0; ++i<20;) {
-      aaaa.removeFirst();
-    }
-    System.out.println(aaaa);
   }
 }
