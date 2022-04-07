603082f8f8631cb81eec7353f1389bfa9126ca02
Jing Qian <jqian30@stuy.edu>
2022-04-07 15:30:28 -0400

    rewrote frontier class

--- 09-Treeburning/Frontier.java
+++ 09-Treeburning/Frontier.java
@@ -1,27 +1,8 @@
 import java.util.*;
 
-public class Frontier {
-  private Queue<int[]> frontier;
-
-  public Frontier() {
-    frontier = new ArrayDeque<int[]>();
-  }
-
-  public int size() {
-    return frontier.size();
-  }
-
-  public void add(int[] location) {
-    assert location.length == 2; //location is an ordered pair
-    frontier.add(location);
-  }
-
+public class Frontier extends ArrayDeque<int[]> {
   public void add(int x, int y) {
     int[] coords = {x, y};
     add(coords);
-  }
-
-  public int[] remove() {
-    return frontier.remove();
   }
 }
