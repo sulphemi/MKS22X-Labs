e4b7a894271a100c007fc660ed0a3af43a3db4f0
Jing Qian <jqian30@stuy.edu>
2022-04-07 15:35:26 -0400

    getX and getY

--- 09-Treeburning/Frontier.java
+++ 09-Treeburning/Frontier.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class Frontier extends ArrayDeque<int[]> {
+  // Frontier is an extension of ArrayDeque such that it stores int[]
+  // of length 2, representing an ordered pair, x and y respectively
+
   public boolean add(int x, int y) {
     int[] coords = {x, y};
     super.add(coords);
@@ -11,5 +14,13 @@ public class Frontier extends ArrayDeque<int[]> {
     assert coords.length > 2;
     super.add(coords);
     return true;
+  }
+
+  public int getX(int index) {
+    return get(index)[0];
+  }
+
+  public int getY(int index) {
+    return get(index)[1];
   }
 }
